55e3aaa84d43571317d47a64c49976a89b309571
Pavlo Nahurnyi <polusnp@gmail.com>
2024-01-14 22:57:54 +0200

    radio_box_app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./index.css"
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold text-center text-blue-200 bg-blue-900">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
